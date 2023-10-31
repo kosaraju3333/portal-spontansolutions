3a08a7e2b10d7aeb067add2526ec8fd777d0949c
kosaraju3333 <kosaraju3333@gmail.com>
2023-10-31 16:18:57 +0530

    Removed UntarSourcecodefile

--- scripts/DownloadSourceCode_tar.sh
+++ scripts/DownloadSourceCode_tar.sh
@@ -11,4 +11,7 @@ else
    mkdir /home/ubuntu/portal-spontansolutions
 fi
 
-aws s3 cp s3://portal-spontansolutions/portal-spontan.tar.gz /home/ubuntu/portal-spontansolutions/
+cd /home/ubuntu/portal-spontansolutions
+aws s3 cp s3://portal-spontansolutions/portal-spontan.tar.gz /home/ubuntu/portal-spontansolutions
+tar -xvf /home/ubuntu/portal-spontansolutions/portal-spontan.tar.gz
+rm -rf /home/ubuntu/portal-spontansolutions/portal-spontan.tar.gz
