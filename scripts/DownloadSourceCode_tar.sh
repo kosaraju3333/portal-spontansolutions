a3f60dcdce969520a792213460c5f9af8be9a0d6
kosaraju3333 <kosaraju3333@gmail.com>
2023-10-31 16:30:10 +0530

    updated the script

--- scripts/DownloadSourceCode_tar.sh
+++ scripts/DownloadSourceCode_tar.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-Directory="/home/ubuntu/portal-spontansolutions/"
+# Directory="/home/ubuntu/portal-spontansolutions/"
 
-if [ -d "$Directory" ]
-then
-   echo "portal-spontansolutions is already exist"
-   break
-else
-   mkdir /home/ubuntu/portal-spontansolutions
-fi
+# if [ -d "$Directory" ]
+# then
+#    echo "portal-spontansolutions is already exist"
+#    break
+# else
+#    mkdir /home/ubuntu/portal-spontansolutions
+# fi
 
 cd /home/ubuntu/portal-spontansolutions
 aws s3 cp s3://portal-spontansolutions/portal-spontan.tar.gz /home/ubuntu/portal-spontansolutions
